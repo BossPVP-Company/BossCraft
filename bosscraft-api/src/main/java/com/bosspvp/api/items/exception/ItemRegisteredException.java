46f15fcecd69ffe3d0d4e31be90785dfb090c092
FenixRa22 <53151806+FenixRa22@users.noreply.github.com>
2023-05-18 23:44:37 +0200

    clean-up. Removed base-api staff for now, to test the configs first

--- bosscraft-api/src/main/java/com/bosspvp/api/items/exception/ItemRegisteredException.java
+++
@@ -1,9 +0,0 @@
-package com.bosspvp.api.items.exception;
-
-public class ItemRegisteredException extends Exception {
-
-    public ItemRegisteredException() {
-        super("Item with same id already registered!");
-    }
-
-}
