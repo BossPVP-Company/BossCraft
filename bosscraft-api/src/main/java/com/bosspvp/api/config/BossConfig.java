b565bbdf0aaec79baefef5fbf2a8332799a233b8
FenixRa22 <53151806+FenixRa22@users.noreply.github.com>
2023-05-26 22:51:12 +0200

    update: BossConfig#asSection for skills lib

--- bosscraft-api/src/main/java/com/bosspvp/api/config/BossConfig.java
+++ bosscraft-api/src/main/java/com/bosspvp/api/config/BossConfig.java
@@ -1,13 +1,39 @@
 package com.bosspvp.api.config;
 
 import eu.okaeri.configs.OkaeriConfig;
+import eu.okaeri.configs.yaml.bukkit.YamlBukkitConfigurer;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-/**
- * Config class.
- * Added for easier implementation of required methods later
- */
+import java.lang.reflect.Field;
+
+
 public class BossConfig extends OkaeriConfig {
+    private YamlConfiguration handle;
 
+    public BossConfig(){
+        if(!(getConfigurer() instanceof YamlBukkitConfigurer)) {
+            throw new IllegalArgumentException("Tried to apply unsupported okaeri configurer for BossConfig");
+        }
+        try {
+            Field configField = getConfigurer().getClass().getDeclaredField("config");
+            configField.setAccessible(true);
+            this.handle = (YamlConfiguration) configField.get(getConfigurer());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
+    public boolean has(@NotNull String path){
+        return get(path) != null;
+    }
 
+    public @Nullable ConfigurationSection getSubsection(@NotNull String path){
+        return handle.getConfigurationSection(path);
+    }
+    public @NotNull ConfigurationSection asSection(){
+        return handle;
+    }
 }
