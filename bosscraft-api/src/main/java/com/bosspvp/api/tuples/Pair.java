c1fe5b1695cba50e9a00b6caf057ab42401a62e9
FenixRa22 <53151806+FenixRa22@users.noreply.github.com>
2023-05-27 10:28:22 +0200

    update: simplified tuples via lombok

--- bosscraft-api/src/main/java/com/bosspvp/api/tuples/Pair.java
+++ bosscraft-api/src/main/java/com/bosspvp/api/tuples/Pair.java
@@ -1,60 +1,27 @@
 package com.bosspvp.api.tuples;
 
-import org.jetbrains.annotations.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Two values.
+ * Two nullable values.
  *
  * @param <A> The first value type.
  * @param <B> The second value type.
  */
+@AllArgsConstructor
 public class Pair<A, B> {
     /**
      * The first item in the tuple.
      */
-    @Nullable
+    @Nullable @Getter @Setter
     private A first;
 
     /**
      * The second item in the tuple.
      */
-    @Nullable
+    @Nullable @Getter @Setter
     private B second;
-
-    /**
-     * Create a pair of values.
-     *
-     * @param first  The first item in the pair.
-     * @param second The second item in the pair.
-     */
-    public Pair(@Nullable final A first,
-                @Nullable final B second) {
-        this.first = first;
-        this.second = second;
-    }
-
-
-    public A component1() {
-        return first;
-    }
-    public B component2() {
-        return second;
-    }
-
-
-    public @Nullable A getFirst() {
-        return this.first;
-    }
-    public @Nullable B getSecond() {
-        return this.second;
-    }
-
-
-    public void setFirst(@Nullable final A first) {
-        this.first = first;
-    }
-    public void setSecond(@Nullable final B second) {
-        this.second = second;
-    }
 }
--- bosscraft-api/src/main/java/com/bosspvp/api/tuples/Triplet.java
+++ bosscraft-api/src/main/java/com/bosspvp/api/tuples/Triplet.java
@@ -1,80 +1,35 @@
 package com.bosspvp.api.tuples;
 
-import org.jetbrains.annotations.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Three values.
+ * Three nullable values.
  *
  * @param <A> The first value type.
  * @param <B> The second value type.
  * @param <C> The third value type.
  */
+@AllArgsConstructor
 public class Triplet<A,B,C> {
 
     /**
      * The first item in the tuple.
      */
-    @Nullable
+    @Nullable @Getter @Setter
     private A first;
 
     /**
      * The second item in the tuple.
      */
-    @Nullable
+    @Nullable @Getter @Setter
     private B second;
 
     /**
      * The third item in the tuple.
      */
-    @Nullable
+    @Nullable @Getter @Setter
     private C third;
-
-    /**
-     * Create a triplet of values.
-     *
-     * @param first  The first item in the pair.
-     * @param second The second item in the pair.
-     * @param third The third item in the pair.
-     */
-    public Triplet(@Nullable final A first,
-                   @Nullable final B second,
-                   @Nullable final C third) {
-        this.first = first;
-        this.second = second;
-        this.third = third;
-    }
-
-
-    public A component1() {
-        return first;
-    }
-    public B component2() {
-        return second;
-    }
-    public C component3() {
-        return third;
-    }
-
-
-    public @Nullable A getFirst() {
-        return this.first;
-    }
-    public @Nullable B getSecond() {
-        return this.second;
-    }
-    public @Nullable C getThird() {
-        return this.third;
-    }
-
-
-    public void setFirst(@Nullable final A first) {
-        this.first = first;
-    }
-    public void setSecond(@Nullable final B second) {
-        this.second = second;
-    }
-    public void setThird(@Nullable final C third) {
-        this.third = third;
-    }
 }
