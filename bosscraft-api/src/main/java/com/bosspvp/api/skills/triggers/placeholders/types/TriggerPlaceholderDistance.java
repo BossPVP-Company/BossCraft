5453af9c368d54b55b48106166b4541dbf50d4de
FenixRa22 <53151806+FenixRa22@users.noreply.github.com>
2023-06-09 10:47:30 +0200

    Added implementation #3

--- bosscraft-api/src/main/java/com/bosspvp/api/skills/triggers/placeholders/types/TriggerPlaceholderDistance.java
+++ bosscraft-api/src/main/java/com/bosspvp/api/skills/triggers/placeholders/types/TriggerPlaceholderDistance.java
@@ -2,11 +2,14 @@ package com.bosspvp.api.skills.triggers.placeholders.types;
 
 import com.bosspvp.api.BossPlugin;
 import com.bosspvp.api.placeholders.InjectablePlaceholder;
+import com.bosspvp.api.placeholders.types.injectable.StaticPlaceholder;
 import com.bosspvp.api.skills.triggers.TriggerData;
 import com.bosspvp.api.skills.triggers.placeholders.TriggerPlaceholder;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class TriggerPlaceholderDistance extends TriggerPlaceholder {
     public TriggerPlaceholderDistance(@NotNull BossPlugin plugin) {
@@ -15,6 +18,14 @@ public class TriggerPlaceholderDistance extends TriggerPlaceholder {
 
     @Override
     public Collection<InjectablePlaceholder> createPlaceholders(TriggerData data) {
-        return null;
+        var victim = data.victim();
+        if(victim==null) return new ArrayList<>();
+        var player = data.player();
+        if(player==null) return new ArrayList<>();
+
+        return List.of(
+                new StaticPlaceholder("distance",
+                        ()-> String.valueOf(player.getLocation().toVector().distance(victim.getLocation().toVector())))
+        );
     }
 }
