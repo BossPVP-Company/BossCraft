c79ddf1dbe5b05be0a7e43f5f26dedeba291044a
FenixRa22 <53151806+FenixRa22@users.noreply.github.com>
2023-06-09 11:21:36 +0200

    Added condition types #3

--- bosscraft-api/src/main/java/com/bosspvp/api/skills/conditions/ConditionsRegistry.java
+++ bosscraft-api/src/main/java/com/bosspvp/api/skills/conditions/ConditionsRegistry.java
@@ -1,25 +1,23 @@
 package com.bosspvp.api.skills.conditions;
 
-import com.bosspvp.api.BossAPI;
 import com.bosspvp.api.BossPlugin;
 import com.bosspvp.api.config.Config;
 import com.bosspvp.api.registry.Registry;
+import com.bosspvp.api.skills.conditions.types.ConditionHasPermission;
+import com.bosspvp.api.skills.conditions.types.ConditionInAir;
+import com.bosspvp.api.skills.conditions.types.ConditionInBiome;
+import com.bosspvp.api.skills.conditions.types.ConditionInBlock;
 import com.bosspvp.api.skills.violation.ConfigViolation;
 import com.bosspvp.api.skills.violation.ViolationContext;
-import org.bukkit.entity.Boss;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class ConditionsRegistry extends Registry<Condition<?>> {
     private final BossPlugin plugin;
-
-    public ConditionsRegistry(BossPlugin plugin){
-        this.plugin = plugin;
-    }
-
     
 
     /**
@@ -37,7 +35,6 @@ public class ConditionsRegistry extends Registry<Condition<?>> {
     public @Nullable ConditionBlock<?> compile(Config cfg, ViolationContext context){
 
         //@TODO cfg.separatorAmbivalent();
-
         Config config = cfg;
 
         var condition = get(config.getString("id"));
@@ -79,4 +76,12 @@ public class ConditionsRegistry extends Registry<Condition<?>> {
         }
 
     }
+    public ConditionsRegistry(BossPlugin plugin){
+        register(new ConditionInAir(plugin));
+        register(new ConditionInBiome(plugin));
+        register(new ConditionHasPermission(plugin));
+        register(new ConditionInBlock(plugin));
+        this.plugin = plugin;
+    }
+
 }
---
+++ bosscraft-api/src/main/java/com/bosspvp/api/skills/conditions/types/ConditionHasPermission.java
@@ -0,0 +1,23 @@
+package com.bosspvp.api.skills.conditions.types;
+
+import com.bosspvp.api.BossPlugin;
+import com.bosspvp.api.config.Config;
+import com.bosspvp.api.skills.Compilable;
+import com.bosspvp.api.skills.conditions.Condition;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class ConditionHasPermission extends Condition<Compilable.NoCompileData> {
+    public ConditionHasPermission(@NotNull BossPlugin plugin) {
+        super(plugin,"has_permission");
+        setArguments(it->{
+            it.require("permission", "You must specify the permission!");
+        });
+    }
+
+    @Override
+    public boolean isMet(Player player, Config config, NoCompileData compileData) {
+        return player.hasPermission(config.getString("permission"));
+    }
+
+}
