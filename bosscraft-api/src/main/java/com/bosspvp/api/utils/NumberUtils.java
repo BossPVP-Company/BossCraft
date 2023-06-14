1bbe4afb4e183512b1f3781885cd6e5a63bc7eb5
FenixRa22 <53151806+FenixRa22@users.noreply.github.com>
2023-06-14 14:39:19 +0200

    numerals utils #3

---
+++ bosscraft-api/src/main/java/com/bosspvp/api/utils/NumberUtils.java
@@ -0,0 +1,63 @@
+package com.bosspvp.api.utils;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class NumberUtils {
+    private static final TreeMap<Integer, String> NUMERALS = new TreeMap<>();
+
+    static {
+        NUMERALS.put(1000, "M");
+        NUMERALS.put(900, "CM");
+        NUMERALS.put(500, "D");
+        NUMERALS.put(400, "CD");
+        NUMERALS.put(100, "C");
+        NUMERALS.put(90, "XC");
+        NUMERALS.put(50, "L");
+        NUMERALS.put(40, "XL");
+        NUMERALS.put(10, "X");
+        NUMERALS.put(9, "IX");
+        NUMERALS.put(5, "V");
+        NUMERALS.put(4, "IV");
+        NUMERALS.put(1, "I");
+    }
+
+    /**
+     * Get Roman Numeral from number.
+     *
+     * @param number The number to convert.
+     * @return The number, converted to a roman numeral.
+     */
+    @NotNull
+    public static String toNumeral(final int number) {
+        if (number >= 1 && number <= 4096) {
+            int l = NUMERALS.floorKey(number);
+            if (number == l) {
+                return NUMERALS.get(number);
+            }
+            return NUMERALS.get(l) + toNumeral(number - l);
+        } else {
+            return String.valueOf(number);
+        }
+    }
+
+    /**
+     * Get number from roman numeral.
+     *
+     * @param numeral The numeral to convert.
+     * @return The number, converted from a roman numeral.
+     */
+    public static int fromNumeral(@NotNull final String numeral) {
+        if (numeral.isEmpty()) {
+            return 0;
+        }
+        for (Map.Entry<Integer, String> entry : NUMERALS.descendingMap().entrySet()) {
+            if (numeral.startsWith(entry.getValue())) {
+                return entry.getKey() + fromNumeral(numeral.substring(entry.getValue().length()));
+            }
+        }
+        return 0;
+    }
+}
